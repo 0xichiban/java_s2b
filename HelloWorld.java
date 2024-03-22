5aa2a33a97ec7c8f2d31ef4d0c8a97916334b011
0xichiban <cofeokudasai@gmail.com>
2024-03-22 22:52:48 +0900

    acnsascn

---
+++ HelloWorld.java
@@ -0,0 +1,27 @@
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("Hello World!!");
+
+        // 变量的验证代码
+        byte abyte = 0xF;
+        System.out.println(abyte);
+        short ashort = 0x7FFF;
+        System.out.println(ashort);
+        int aint=0xFFFFFFFF;
+        System.out.println(aint);
+        double adouble = 0xFFFFFFFF;
+        System.out.println(adouble);
+
+        // 数组变量
+        int[] arrayes = new int[20];
+        for(byte i=0; i<arrayes.length; i++){
+            System.out.println(arrayes[i]);
+        }
+
+        int[] arrayes2 = new int[22];
+        for(byte i=0; i<arrayes2.length; i++){
+            arrayes2[i]
+        }
+
+    }
+}
